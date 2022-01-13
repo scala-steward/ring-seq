a1337b0bfee68ebe727a00007fd800108412fe3d
Mario Càllisto <mario.callisto@gmail.com>
2022-01-13 16:05:24 +0100

    Improved rotations and reflections

--- src/test/scala/RotationsReflectionsSpec.scala
+++ src/test/scala/RotationsReflectionsSpec.scala
@@ -1,3 +1,7 @@
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Test.check
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
@@ -60,21 +64,46 @@ class RotationsReflectionsSpec extends AnyFlatSpec with RingVector with should.M
   }
 
   it can "be the rotation of another Vector" in {
-    Vector.empty.isRotationOf(Vector.empty) shouldBe true
-    v.isRotationOf(v) shouldBe true
     v.isRotationOf(Vector(3, 4, 5, 1, 2)) shouldBe true
     v.allRotations.forall(v.isRotationOf) shouldBe true
   }
 
   it can "be the reflection of another Vector" in {
-    v.isReflectionOf(v) shouldBe true
     v.isReflectionOf(Vector(1, 5, 4, 3, 2)) shouldBe true
   }
 
   it can "be the rotation or reflection of another Vector" in {
-    Vector.empty.isRotationOrReflectionOf(Vector.empty) shouldBe true
-    v.isRotationOrReflectionOf(v) shouldBe true
     v.isRotationOrReflectionOf(Vector(3, 2, 1, 5, 4)) shouldBe true
     v.allRotationsAndReflections.forall(v.isRotationOrReflectionOf) shouldBe true
   }
+
+  val vectorIntGen: Gen[Vector[Int]] =
+    for
+      list <- arbitrary[List[Int]]
+    yield list.toVector
+
+  "All rotations of a Vector" must "contain itself" in {
+    check(
+      forAll(vectorIntGen)(vector => vector.allRotations.contains(vector))
+    )
+  }
+
+  "All rotations and reflections of a Vector" must "contain itself" in {
+    check(
+      forAll(vectorIntGen)(vector => vector.allRotationsAndReflections.contains(vector))
+    )
+  }
+
+  "A Vector" must "always be the rotation of itself" in {
+    check(
+      forAll(vectorIntGen)(vector => vector.isRotationOf(vector))
+    )
+  }
+
+  it must "always be the rotation or reflection of itself" in {
+    check(
+      forAll(vectorIntGen)(vector => vector.isRotationOrReflectionOf(vector))
+    )
+  }
+
 }
